af7d2e906e62b25f3068ebd4b1d5ecf20330ca4e
Vyacheslav Serov <scandale89@gmail.com>
2019-08-22 20:15:22 +0300

    First commit

---
+++ src/main/java/com/geekbrains/client/Controller.java
@@ -0,0 +1,111 @@
+package com.geekbrains.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Controller {
+    @FXML
+    TextField msgField;
+
+    @FXML
+    TextArea chatArea;
+
+    @FXML
+    HBox bottomPanel;
+
+    @FXML
+    HBox upperPanel;
+
+    @FXML
+    javafx.scene.control.TextField loginfield;
+
+    @FXML
+    PasswordField passwordField;
+
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADDRESS = "localhost";
+    final int PORT = 8189;
+    boolean isAuthorized;
+
+    public void setAuthorized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+        if (!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+        }
+    }
+    public void connect() {
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            setAuthorized(false);
+            new Thread(() -> {
+                try{
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/authok")) {
+                            setAuthorized(true);
+                            break;
+                        } else {
+                            chatArea.appendText(str + "/n");
+                        }
+                    }
+                    while(true) {
+                        String str = in.readUTF();
+                        if (str.equals("/serverclosed")) break;
+                        chatArea.appendText(str + "/n");
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    setAuthorized(false);
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(msgField.getText());
+            msgField.clear();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void tryToAuth(){
+        connect();
+        try {
+            out.writeUTF("/auth "+ loginfield.getText() + " " + passwordField.getText());
+            loginfield.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
